48bd90d40946169f60df66de52330bba3a760dfa
Puspita Bhattacharyya <23f2005100@ds.study.iitm.ac.in>
2026-04-28 14:39:11 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,9 +1,8 @@
 import sys
-import os        # unsorted imports
+
 
 def calculate():
-    x = 100      # unused variable (ruff will complain)
-    print("Hello World")  
-    # There is trailing whitespace after this line 
+    print("Hello World")
+
 
 print("Done")
