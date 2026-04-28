0f7c13b319da579919808e9ab1f9a385b8960b73
Puspita Bhattacharyya <23f2005100@ds.study.iitm.ac.in>
2026-04-28 14:35:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello, Ruff!")
+import sys
+import os        # unsorted imports
+
+def calculate():
+    x = 100      # unused variable (ruff will complain)
+    print("Hello World")  
+    # There is trailing whitespace after this line 
+
+print("Done")
